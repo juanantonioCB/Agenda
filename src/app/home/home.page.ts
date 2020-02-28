034744b1abf0b391cc7fdc3eb9960e6557c1fb60
Juan Antonio Cabello <juanantonio186@hotmail.com>
2020-02-28 14:58:42 +0100

    copia

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseDBService } from '../services/firebase-db.service';
+import { Task } from '../model/Task';
+import { UiComponent } from '../ui/ui.component';
 
 
 @Component({
@@ -8,6 +11,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tasks: Task[];
+  constructor(private db: FirebaseDBService, private ui:UiComponent) { }
+
+  async ngOnInit() {
+    (await this.db.getTasks('user1')).subscribe(res => {
+      this.tasks = res;
+    });
+  }
+
+  edit(id:string){
+    console.log(id);
+  }
+  
+  async delete(id:string){
+    await this.ui.presentLoading();
+    this.db.removeTask(id,'user1').then(async r=>{
+      await this.ui.presentToast('Tarea eliminada correctamente','success');
+    }).catch(async err=>{
+      await this.ui.presentToast('Ha ocurrido un error al eliminar','danger');
+    });
+     await this.ui.hideLoading();
+  }
 
 }
